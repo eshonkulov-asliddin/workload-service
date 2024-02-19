94b3b418aad6f0c321a3341191336adf339946e4
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-02-19 16:25:02 +0500

    Update path

--- src/main/java/dev/gym/workloadservice/controller/util/RestApiConst.java
+++ src/main/java/dev/gym/workloadservice/controller/util/RestApiConst.java
@@ -2,6 +2,6 @@ package dev.gym.workloadservice.controller.util;
 
 public class RestApiConst {
 
-    public static final String API_ROOT_PATH = "/workload-service";
+    public static final String API_ROOT_PATH = "/workload";
     public static final String TRAINER_WORKLOAD_API_ROOT_PATH = API_ROOT_PATH + "/trainers";
 }
