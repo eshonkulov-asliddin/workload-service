8cf577f6238d7b188ddcfd459b1a00a3e206899e
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-02-19 11:18:18 +0500

    Update jwt

--- src/test/java/dev/gym/workloadservice/security/jwt/JwtUtilTest.java
+++ src/test/java/dev/gym/workloadservice/security/jwt/JwtUtilTest.java
@@ -10,13 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @ExtendWith(MockitoExtension.class)
 class JwtUtilTest {
 
+    private String secret = "2dae84f846e4f4b158a8d26681707f4338495bc7ab68151d7f7679cc5e56202dd3da0d356da007a7c28cb0b780418f4f3246769972d6feaa8f610c7d1e7ecf6a";
+    private long expirationTime = 3600000L;
     private JwtUtil jwtUtil;
     private String token;
 
     @BeforeEach
     void setUp(){
         jwtUtil = new JwtUtil();
-        jwtUtil.setSecret("2dae84f846e4f4b158a8d26681707f4338495bc7ab68151d7f7679cc5e56202dd3da0d356da007a7c28cb0b780418f4f3246769972d6feaa8f610c7d1e7ecf6a");
+        jwtUtil.setSecret(secret);
+        jwtUtil.setExpirationTime(expirationTime);
         token = jwtUtil.generateToken("username");
     }
 
