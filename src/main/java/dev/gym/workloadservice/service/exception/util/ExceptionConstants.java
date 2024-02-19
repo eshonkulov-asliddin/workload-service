ff575559a4b7d683329c9be34e9789c976d28b2d
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-02-19 14:01:06 +0500

    Add custom exception

---
+++ src/main/java/dev/gym/workloadservice/service/exception/util/ExceptionConstants.java
@@ -0,0 +1,6 @@
+package dev.gym.workloadservice.service.exception.util;
+
+public class ExceptionConstants {
+
+    public static final String TRAINER_NOT_FOUND = "Trainer not found with username: %s";
+}
