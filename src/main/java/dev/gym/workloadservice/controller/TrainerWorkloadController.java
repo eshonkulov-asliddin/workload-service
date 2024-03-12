485bd23b294f882c59e30a4cb761c2a9e6ca4ce2
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-03-12 13:52:03 +0500

    Return response entity

--- src/main/java/dev/gym/workloadservice/controller/TrainerWorkloadController.java
+++ src/main/java/dev/gym/workloadservice/controller/TrainerWorkloadController.java
@@ -6,6 +6,7 @@ import dev.gym.workloadservice.dto.TrainersTrainingSummaryDTO;
 import dev.gym.workloadservice.service.TrainersTrainingSummaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequiredArgsConstructor
 public class TrainerWorkloadController {
@@ -23,8 +22,8 @@ public class TrainerWorkloadController {
     private final TrainersTrainingSummaryService trainersTrainingSummaryService;
 
     @GetMapping(RestApiConst.TRAINER_WORKLOAD_MONTHLY_REPORT_API_ROOT_PATH + "/{username}")
-    public Optional<TrainersTrainingSummaryDTO> calculateMonthlyReport(@PathVariable(value = "username") String username) {
-        return trainersTrainingSummaryService.getTrainingSummaryByUsername(username);
+    public ResponseEntity<TrainersTrainingSummaryDTO> calculateMonthlyReport(@PathVariable(value = "username") String username) {
+        return ResponseEntity.of(trainersTrainingSummaryService.getTrainingSummaryByUsername(username));
     }
 
     @PostMapping(RestApiConst.TRAINER_WORKLOAD_API_ROOT_PATH)
