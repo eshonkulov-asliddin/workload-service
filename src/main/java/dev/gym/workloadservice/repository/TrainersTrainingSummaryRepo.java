91b9ed734953d7c8037d6f1d2ed031326591cb9c
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-03-12 14:03:00 +0500

    Removed unnecessary query

--- src/main/java/dev/gym/workloadservice/repository/TrainersTrainingSummaryRepo.java
+++ src/main/java/dev/gym/workloadservice/repository/TrainersTrainingSummaryRepo.java
@@ -1,7 +1,6 @@
 package dev.gym.workloadservice.repository;
 
 import dev.gym.workloadservice.model.TrainersTrainingSummary;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface TrainersTrainingSummaryRepo extends CrudRepository<TrainersTrainingSummary, String> {
 
-    @Query("{ trainerUsername : '?0' }")
     Optional<TrainersTrainingSummary> findSummaryByTrainerUsername(String trainerUsername);
 
 }
